51b0f0938ab11af8f7d589addf14113263b8fe7c
Luiz Henrique Cassettari <ricaun@gmail.com>
2025-04-24 11:54:06 -0300

    Add Samples

---
+++ TestProjectNoAdapter/TestFixtureNoRevitAttribute.cs
@@ -0,0 +1,29 @@
+﻿#if NETFRAMEWORK
+
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace TestProjectNoAdapter
+{
+    [TestFixtureNoRevit]
+    public class DummyTestToIgnoreRevitTest
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.Pass();
+        }
+    }
+
+    public class TestFixtureNoRevitAttribute : TestFixtureAttribute
+    {
+        public TestFixtureNoRevitAttribute() : base()
+        {
+            if (AppDomain.CurrentDomain.GetAssemblies().Any(e => e.GetName().Name.Contains("RevitTest")))
+                throw new NotSupportedException("This module should not be used by RevitTest.");
+        }
+    }
+}
+
+#endif
---
+++ TestProjectRevitAdapter/SampleTests.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+using System.Diagnostics;
+
+namespace TestProjectRevitAdapter
+{
+    public class SampleTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            System.Console.WriteLine(Process.GetCurrentProcess().ProcessName);
+            Assert.Pass();
+        }
+
+        [TestCase(1)]
+        [TestCase(2)]
+        public void SampleTestCase(int value)
+        {
+            Assert.Pass();
+        }
+    }
+}
+
+
